d0b58e255b5cb6b9b06b2e21e0d08803020b3f73
Pawel <pawel.jachimowskipl@gmail.com>
2021-03-18 15:33:11 +0100

    tak2

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+    <head> 
+    </head>
+        <body>
+                <?php 
+                echo("<h1>docker-działa</h2>");
+                echo("<h2>docker-(...)");
+                require("connect.php");
+                    $sql="SELECT*FROM pracownicy";
+                    echo("<h2>Polecenie: Wszyscy pracownicy </h2>".$sql);
+                    $result=$conn->query($sql);
+                        echo("<table border=1>");
+                        echo("<th>id_pracownicy</th>");
+                        echo("<th>imie</th>");
+                        echo("<th>dzial</th>");
+                        echo("<th>zarobki</th>");
+                            while($row=$result->fetch_assoc()){
+                                echo("<tr>");
+                                   echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row['dzial']."</td><td>".$row["zarobki"]."</td>");
+                                echo("</tr>");
+                            };
+                         echo("</table>");
+                   
+                ?>
+        </body>
+</html>
